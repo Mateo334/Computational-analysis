febae2d9685839140c7f48a132c66fa5e6d75e9c
Matěj Ruml <matejruml@gmail.com>
2025-01-21 23:17:00 +0100

    randomness tests

--- PRNG.py
+++ PRNG.py
@@ -1,1 +1,29 @@
 "Pseudo-random generation"  
+import math
+import time as t
+import re
+import matplotlib.pyplot as plt
+# N should refer to the number of numbers to generate
+N = 40
+def middle_square(seed = int((re.sub(r'\.(0*)', '', str(t.time())))[-6::])): 
+    #The seed is biased and may not range all values
+    ar = []
+    for i in range(len(seed)):
+        # x = len(str(seed))
+        x = len(str(seed[i]))
+        s = seed[i]**2
+        while(len(str(s)) < 2*x): s*=10
+        ar.append(int(str(s)[math.ceil(x*0.5):math.floor(x*1.5)]))
+    return ar
+def lehmer_gen(seed = int((re.sub(r'\.(0*)', '', str(t.time())))[-6::])):
+    ar = []
+    r = 10
+    a = 7**5
+    m = 2**31-1 
+    for i in range(len(seed)):
+        for j in range(r):
+            seed[j] = a*seed[j]%m
+        ar.append(seed[j])
+    return ar
+
+
---
+++ random_checking.py
@@ -0,0 +1,33 @@
+from PRNG import *
+
+
+def time_sampling(N):
+    """Samples pseudo-random seeds of up to 1e6."""
+    x = []
+    for i in range(N):
+        num = int((re.sub(r'\.(0*)', '', str(t.time())))[-6::])
+        while(num<10**5):
+            num = int((re.sub(r'\.(0*)', '', str(t.time())))[-6::]) #6 means up to 10**6
+        # Here you can modify the range
+        x.append(num)
+        t.sleep(1e-10)
+    return x
+N = 100
+def plotter(N, f):
+    """Plots the data wrt time sampled seed of number N and function f."""
+    x = time_sampling(N)
+    plt.scatter([i for i in range(N)], [f(a) for a in x])
+    plt.show()
+# plotter(N, middle_square)
+
+def birthday_spacings(data):
+    """checks the distance between the points. May fit it to an exponential."""
+    data = sorted(data)
+    dists = []
+    for i in range(0, len(data)-1):
+        dists.append(abs(data[i]-data[i+1]))
+    dists = sorted(dists)[::-1]
+    plt.scatter([i for i in range(len(dists))], dists)
+    plt.show()
+birthday_spacings(lehmer_gen(time_sampling(N)))    
+# birthday_spacings(middle_square(time_sampling(N)))    
