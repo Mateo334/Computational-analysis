b8524a59cc134c0a068a438e09218a7123024fbf
Matej <matejruml@gmail.com>
2025-12-19 22:44:07 +0100

    Added gitignore

--- Tournament_prediction/Data/extract_data.py
+++ Tournament_prediction/Data/extract_data.py
@@ -3,6 +3,23 @@ import pandas as pd
 import os
 
 games = []
+# path = "twic1617"
+# with open(f"Data/{path}g/{path}.pgn", encoding="utf-8") as pgn:
+#     while True:
+#         game = chess.pgn.read_game(pgn)
+#         if game is None:
+#             break
+#         headers = game.headers
+#         games.append({
+#             "Date": headers.get("Date"),
+#             "White": headers.get("White"),
+#             "Black": headers.get("Black"),
+#             "Result": headers.get("Result"),
+#             "WhiteElo": headers.get("WhiteElo"),
+#             "BlackElo": headers.get("BlackElo")
+#         })
+
+# pd.DataFrame(games).to_csv("Data/Results/{path}.csv", index=False)
 path = "twic1617"
 with open(f"Data/{path}g/{path}.pgn", encoding="utf-8") as pgn:
     while True:
@@ -18,6 +35,7 @@ with open(f"Data/{path}g/{path}.pgn", encoding="utf-8") as pgn:
             "WhiteElo": headers.get("WhiteElo"),
             "BlackElo": headers.get("BlackElo"),
             "Variation": headers.get("Variation")
+            # "Game": headers.get("") #Missing the played position
         })
 
-pd.DataFrame(games).to_csv("Data/Results/{path}.csv", index=False)
+pd.DataFrame(games).to_csv(f"Data/Games/{path}.csv", index=False)
--- Tournament_prediction/Leela_v_1/predict.py
+++ Tournament_prediction/Leela_v_1/predict.py
@@ -9,35 +9,26 @@ player_elos  = pd.read_csv("Data/Elo/cur_elo.csv")
 
 with open("Leela_v_1/tournament_names.txt", 'r') as f:
     tournament = [int(x.strip()) for x in f.readlines()]
-    
-# def play_game(a,b):
-#     """Plays the game and prints the outcome -> 1 = a won."""
-#     w = 1/(1+np.pow(10,(b-a)/400))
-#     if(random.uniform(0,1)<w):
-#         return 1
-#     return 0
 
-def single_elim(players, player_elos): #Not shuffled
-    # n = len(players)
+def single_elim(players, player_elos, player_prob): #Not shuffled
     outcome = []
     player_in_tournament = players.copy()
     player_elos_in_tournament = player_elos.copy()
-    # print(player_in_tournament)
-    # print(player_elos_in_tournament)
     d = {idx: value for idx, value in enumerate(player_in_tournament)}
     while len(player_in_tournament)>1:
         n = len(player_in_tournament)
         ind = []
         for i in range(0,n,2):
-            if(random.uniform(0,1)< (1/(1+np.pow(10,(player_elos_in_tournament[i+1]-player_elos_in_tournament[i])/400)))):
+            #can precompute this matrix of mutual matches
+            # print(player_elos)
+            val = player_prob[i][i+1]#(1/(1+np.pow(10,(player_elos_in_tournament[i+1]-player_elos_in_tournament[i])/400)))
+            print("Playing: ", player_elos[i+1], player_elos[i], val, i)
+            if(random.uniform(0,1)< val):
                 ind.append(i+1)
-                # outcome.append(player_in_tournament[i+1])
             else:
                 ind.append(i)
-                # outcome.append(player_in_tournament[i])
         ind = sorted(ind, reverse=True)
         for u in ind:
-            # return
             outcome.append(player_in_tournament.pop(u))
             player_elos_in_tournament.pop(u)
     # print(player_in_tournament[0] , "Has won the tournament")
@@ -47,37 +38,58 @@ def single_elim(players, player_elos): #Not shuffled
     return outcome[::-1]
 
 
-def simulate_tournament():
+def simulate_tournament(N):
+    """Plays a whole tournament."""
+    
     players = player_elos[player_elos["Player"].isin(tournament)]
-    # print(len(players))
-    # players_elos = player_elos[player_elos["Player"]==tournament[0]]["Elo"]
+    player_in_tour_elos = players["Elo"].tolist()
     d = {player_id: i for i,player_id in enumerate(players["Player"])}
     print("Player elos: ",players["Elo"].tolist())
     print("Player id: ",players["Player"].tolist())
     player_pos = np.zeros((len(players),len(players)))
-    # print(player_pos, d)
-    # print("Outcome", random_elim(tournament,players["Elo"].tolist(), [], 1))
-    # print("Outcome", single_elim(tournament,players["Elo"].tolist()))
     
-    # print(results)
-    
-    for i in range(100000):
-        # print(tournament, players["Elo"].tolist())
-        # print(len(players))
-        
+    n = len(players)
+    player_prob = np.zeros((n,n))
+    for i in range(n):
+        for j in range(i):
+            val = 1/(1+np.pow(10,(player_in_tour_elos[j]-player_in_tour_elos[i])/400))
+            player_prob[i][j] = val
+            player_prob[j][i] = 1-val
+    # player_prob
+    # print(player_prob)
+    # print(player_in_tour_elos)
+            
+    # return 
+    print(player_prob)
+    for i in range(N):
         indices = list(range(len(players)))
         random.shuffle(indices)
-        results = single_elim([tournament[v] for v in indices],[players["Elo"].tolist()[v] for v in indices])
-        # print(results)
-        # print(results)
-        # print(random.uniform(0,1))
+        results = single_elim([tournament[v] for v in indices],[player_in_tour_elos[v] for v in indices], player_prob)
         for j,res in enumerate(results):
             # print(j, d[res])
             player_pos[d[res]][j] +=1
-    print(player_pos)
+    return player_pos
     # print(players)
     
         
-simulate_tournament()
+
         
-        
+def compute_probability_tournament(player_pos):
+    #very basic, using marginal probs.
+    #Assuming independence across positions
+    player_pos_matrix = np.matrix(player_pos)
+    n = np.sum(player_pos_matrix) // len(player_pos_matrix)
+    player_pos_matrix /=n
+    prob = 1
+    player_assigned = []
+    ind = 0
+    for i in range(len(player_pos_matrix)): #Iterating over positions
+        ind = np.argmax(player_pos_matrix[:,i])
+        prob*=player_pos_matrix[ind, i]
+        player_assigned.append(ind)
+        player_pos_matrix[ind,:] =0
+    
+    print("The most likely probability is : ", prob, player_assigned)
+    
+player_pos = simulate_tournament(1)
+compute_probability_tournament(player_pos)
